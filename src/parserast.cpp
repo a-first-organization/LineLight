c82e6e0c42c75f2a3298d257f41f064aff7419c2
The Good Gamer <felipebastiangomesdeaguiar@gmail.com>
2026-02-24 19:49:28 -0300

    ending the shift for now, renamed blender.hpp to lexerparserheaders.hpp

--- src/parserast.cpp
+++ src/parserast.cpp
@@ -93,3 +93,83 @@ static std::unique_ptr<ExprAST> ParseNumberExpr()
     getNextToken(); // "consume the number" ~someone at llvm deciding what to put as a comment
     return std::move(Result);
 }
+
+// parse the parent expr, aka the ParseParenExpr static function
+static std::unique_ptr<ExprAST> ParseParenExpr()
+{
+    getNextToken();
+    auto V = ParseExpression();
+    if (!V)
+        return nullptr;
+
+    if (CurTok != ')')
+        return LogError("expected ')'");
+    getNextToken(); // eat my ba- no. no. eat the fucking parenthesis, dude. EAT THE FUCKING PARENTHESIS
+    return V;
+}
+
+// identifierexpr
+// ::= identifier
+// ::= identifier (' expression* ')
+// yes that was a whole block. i'm gonna change it to a normal comment. there changed it
+static std::unique_ptr<ExprAST> ParseIdentifierExpr()
+{
+    std::string IdName = IdentifierStr;
+
+    // i hate myself SO BAD... help my soul. again. also eat the identifier. i love llvm /j. nah i'm joking,
+    // but i think the reason why llvm hates me is because i don't have a compiler_commands.json, because THAT'S what caused the errors,
+    // it didnt have the right config. meanwhile c/c++ extension does it just fine, like DUDE????
+    getNextToken(); // eat, my son. eat this identifier i made. well done, just like you like it. i am insane.
+
+    if (CurTok != '(') // simple variable un-reference. i love my ideas
+        return std::make_unique<VariableExprAST>(IdName);
+
+    // "Call." ~someone at llvm.
+    getNextToken(); // gimme food >:( (eat the (, yeah i mean the parenthesis. thats the food)
+    std::vector<std::unique_ptr<ExprAST>> Args;
+    if (CurTok != ')')
+    {
+        while (true)
+        {
+            if (auto Arg = ParseExpression())
+                Args.push_back(std::move(Arg));
+            else
+                return nullptr;
+        }
+
+        if (CurTok == ')')
+            break;
+
+        if (CurTok != ',')
+            return LogError("expected: ')' or ',' in argument list");
+        getNextToken(); // son, eat the comma.
+    }
+
+    // eat the damn parenthesis again.
+    getNextToken();
+
+    return std::make_unique<CallExprAST>(IdName, std::move(Args));
+}
+
+// This routine follows the same style as the ot- oh you thought i was gonna say the llvm? no. because i hate this. 
+// i want my problems to be LastChar again.
+//anyways back to the code
+
+// primary
+// ::= identifierexpr
+// ::= numberexpr
+// ::= parenexpr
+static std::unique_ptr<ExprAST> ParsePrimary() 
+{
+    switch (CurTok)
+    {
+        default:
+        return LogError("unknown token when expecting an expression");
+    case tok_identifier:
+        return ParseIdentifierExpr();
+    case tok_number:
+    return ParseNumberExpr();
+    case '(':
+        return ParseParenExpr();
+    }
+}
